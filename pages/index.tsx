4b508bb2d702ce5957cc88c885401d9b02f20e36
lightin <breval2000@live.fr>
2022-09-01 16:42:53 +0200

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Dash from "./dash";
+
+const Home: NextPage = () => {
+    return (
+        <Dash/>
+    )
+}
+
+export default Home
